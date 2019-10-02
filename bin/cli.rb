4ea1828d8300fbf04599633a861f322f0ea690a0
Joellehelm <joellenhelm@gmail.com>
2019-10-02 09:55:33 -0500

    added categories and difficulty selection

--- bin/cli.rb
+++ bin/cli.rb
@@ -109,12 +109,32 @@ def step_one_to_delete_acc
     end
 end
 
-def quiz_info
+def create_url(category,difficulty)
+    categories = {
+    'General Knowledge': 9,
+    'Music': 12,
+    'Television': 14,
+    'Science & Nature': 17,
+    'Computers': 18,
+    'Mythology': 20,
+    'Sports': 21,
+    'Geography': 22,
+    'History': 23,
+    'Animals': 27
+}
 
-end 
+############ URL API 
 
 
-def selecting_quiz
+    base_url = 'https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple'
+    quiz_url = base_url[0...46] << category << '&difficulty=' << difficulty << '&type=multiple'
+    quiz_url
+end
+
+
+
+def selecting_category
+  category = nil
     puts "Please choose a category."
     puts "Select a number below."
     puts "1. General Knowledge"
@@ -128,16 +148,68 @@ def selecting_quiz
     puts "9. History"
     puts "10. Animals"
    selection = gets.chomp
-   #case statement here.  
-   #then difficulty selection method goes here
+   if selection == "1"
+    category = "9"
+    selecting_difficulty(category)
+   elsif selection == "2"
+    category = "12"
+    selecting_difficulty(category)
+   elsif selection == "3"
+    category = "14"
+    selecting_difficulty(category)
+   elsif selection == "4"
+    category = "17"
+    selecting_difficulty(category)
+   elsif selection == "5"
+    category = "18"
+    selecting_difficulty(category)
+   elsif selection == "6"
+    category = "20"
+    selecting_difficulty(category)
+   elsif selection == "7"
+    category = "21"
+    selecting_difficulty(category)
+   elsif selection == "8"
+    category = "22"
+    selecting_difficulty(category)
+   elsif selection == "9"
+    category = "23"
+    selecting_difficulty(category)
+   elsif selection == "10"
+    category = "27"
+    selecting_difficulty(category)
+   else
+    puts "Please select 1-10."
+    selecting_category
+   end
+    
 end
 
-def selecting_difficulty
+def selecting_difficulty(category)
+    category = category
     puts "Please choose a difficulty."
     puts "Select a number below."
     puts "1. Easy"
     puts "2. Medium"
     puts "3. Hard"
+    difficulty_selection = gets.chomp
+    if difficulty_selection == "1"
+        difficulty = "easy"
+        puts "You got it, Easy Peasy."
+        create_url(category,difficulty)
+    elsif difficulty_selection == "2"
+        difficulty = "medium"
+        puts "Middle of the Road."
+        create_url(category,difficulty)
+    elsif difficulty_selection == "3"
+        difficulty = "hard"
+        puts "Woah. Big Brains Over Here!"
+        create_url(category,difficulty)
+    else
+        puts "Please select 1, 2, or 3."
+        selecting_difficulty
+    end
+
     #play quiz method goes here
 end
 
@@ -151,7 +223,7 @@ def after_logging_in
     puts "4. Delete your account."
 user_choice = gets.chomp
     if user_choice == "1"
-        selecting_quiz
+        selecting_category
     elsif user_choice == "2"
         #view past scores method
     elsif user_choice == "3"
@@ -171,6 +243,15 @@ user_choice = gets.chomp
         puts "========================="
 
         after_logging_in
+    end
+
+
+    def play_quiz
+        correct_answers = []
+        incorrect_answers = []
+        questions = []
+
+        
     end
 
 
